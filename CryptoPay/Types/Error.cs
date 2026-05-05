938e351e1b14026cc32ae529ca5d7bd27858ae9b
Cat42 <bogdan.blood1k@gmail.com>
2026-05-05 23:11:31 +0500

    Add optional Message property to Error model for detailed API error descriptions
    - This field is populated only in rare cases when the API provides additional context about the error. Including it in RequestException improves debugging and provides better insight into specific API responses.

--- CryptoPay/Types/Error.cs
+++ CryptoPay/Types/Error.cs
@@ -19,6 +19,19 @@ public sealed class Error
         this.Name = name;
     }
 
+    /// <summary>
+    /// Create instance of <see cref="Error"/>.
+    /// </summary>
+    /// <param name="code"></param>
+    /// <param name="name"></param>
+    /// <param name="message"></param>
+    public Error(int code, string name, string message)
+    {
+        this.Code = code;
+        this.Name = name;
+        this.Message = message;
+    }
+
     [JsonConstructor]
     private Error() {}
 
@@ -34,15 +47,20 @@ public sealed class Error
     [JsonRequired]
     public string Name { get; set; }
 
+    /// <summary>
+    /// Error message from response.
+    /// </summary>
+    public string Message { get; set; }
+
     /// <inheritdoc/>
     public override bool Equals(object obj)
     {
-        return obj is Error error && Code == error.Code && Name == error.Name;
+        return obj is Error error && Code == error.Code && Name == error.Name && Message == error.Message;
     }
 
     /// <inheritdoc/>
     public override int GetHashCode()
     {
-        return HashCode.Combine(Code, Name);
+        return HashCode.Combine(Code, Name, Message);
     }
 }
